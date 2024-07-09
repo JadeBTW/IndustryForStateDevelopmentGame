d76ba9520887010e620bea4b3aede667706b09e8
JadeBTW <gregzyt@gmail.com>
2024-07-09 12:22:18 +0100

    began integrating pygame and added more comments to main file

--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@ import os
 import Configlib as clib
 cfg = clib.openCfg()
 
+#set internal variables to file config ver
 loggen = cfg["terminal"]["generate-log"]
 verbosity = cfg["terminal"]["write-in-terminal"]
 
@@ -13,14 +14,19 @@ gsl.updateVals(cfg["window-settings"]["win-width"],cfg["window-settings"]["win-h
 
 # set up inhouse log generation and text beautification library
 import Textlib
-log = Textlib.log
+log = Textlib.log #instanciating log object
 Textlib.log.__init__(log)
 Textlib.terminfo('Started Log File and initialised terminal library',log,verbosity,loggen)
 Textlib.terminfo('running terminal formatting test (if colors do not work you may be using a non ANSI compatible system)',log,verbosity,loggen)
 
+#1 print of each terminal type to test coloring in term
 Textlib.terminfo('this is an info entry',log,verbosity,loggen)
 Textlib.termwarn('this is a warn entry',log,verbosity,loggen)
 Textlib.termcritical('this is a critical error entry',log,verbosity,loggen)
+
+#import and init pygame module
+import pygame
+pygame.init()
 
 #exit program and generate log
 if loggen == True:
